3beb3c16c84a822c9acfdfa8a27a9882335af5d4
Roberto Di Remigio <robertodr@users.noreply.github.com>
2021-01-28 12:53:20 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,17 @@
-meson --prefix=%PREFIX% --libdir=lib --buildtype=release --warnlevel=0 -Dbuild_name=conda-forge -Dla_backed=netlib -Ddefault_library=shared
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+meson --prefix=%PREFIX% ^ 
+      --libdir=lib ^ 
+      --buildtype=release ^ 
+      --warnlevel=0 ^ 
+      -Dbuild_name=conda-forge ^
+      -Dla_backend=netlib ^
+      -Ddefault_library=shared ^
+      ..
+if errorlevel 1 exit 1
 
 ninja test install
+if errorlevel 1 exit 1
